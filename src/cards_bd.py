84397d519f581ba2d28fa767b39962c1c6ebb957
Lucas Henry <lucas.henry@enseirb-matmeca.fr>
2019-11-19 10:26:21 +0100

    cards_db: wrote func prototype

---
+++ src/cards_bd.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3.7
+
+def create_db():
+    """Create the card database"""
+    pass
+
+def drop_db():
+    """Delete the card database"""
+    pass
+
+def cards_by_type():
+    """List cards of a certain type."""
+    pass
+
+def cards_not_in_deck():
+    """List cards not in a deck."""
+    pass
+
+def players_collectors():
+    """List players that didn't participated in a game."""
+    pass
+
+def player_list():
+    """List players and the number of cards they own."""
+    pass
+
+def players_by_value():
+    """List player in descending order by the value of their collection."""
+    pass
+
+def cards_in_decks():
+    """List cards and the number of players that use them in their decks."""
+    pass
+
+def player_rare_collectors():
+    """List players who own the maximum of rare card."""
+    pass
+
+def cards_family():
+    """List card family and the caracteristic in which this family has the best level."""
+    pass
